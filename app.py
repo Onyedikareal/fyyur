4ea817c8c8d073aba24d19c2763a3ab9d922402c
Onyedikareal <frankonyeka25@gmail.com>
2022-08-16 03:41:59 +0100

    corrections

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ from config import SQLALCHEMY_DATABASE_URI
 from flask_migrate import Migrate
 from forms import *
 from datetime import datetime
+from models import db, Venue, Artist, Show
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
@@ -22,7 +23,7 @@ from datetime import datetime
 app = Flask(__name__)
 moment = Moment(app)
 app.config.from_object('config')
-db = SQLAlchemy(app)
+db.init_app(app)
 migrate = Migrate(app, db)
 
 # TODO: connect to a local postgresql database
@@ -33,55 +34,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 
 
 
-class Venue(db.Model):
-    __tablename__ = 'Venue'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.String)
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-    website_link = db.Column(db.String(300))
-    seeking_talent = db.Column(db.Boolean, nullable=False, default= False)
-    seeking_description = db.Column(db.String(1000))
-    artists = db.relationship('Artist', secondary='shows',
-      backref=db.backref('venues', lazy=True, uselist= False))
 
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
-
-class Artist(db.Model):
-    __tablename__ = 'Artist'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.String)
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-    website = db.Column(db.String(200))
-    seeking_venue = db.Column(db.Boolean, nullable=False, default= True)
-    seeking_description = db.Column(db.String(1000))
-    
-
-
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
-
-# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
-
-
-
-class Show(db.Model):
-    __tablename__ = 'shows'
-
-    id = db.Column(db.Integer, primary_key=True)
-    start_time = db.Column(db.DateTime, nullable=False, default=datetime.now()) 
-    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
-    artist_id = db.Column( db.Integer, db.ForeignKey('Artist.id'), nullable=False)
 
 #----------------------------------------------------------------------------#
 # Filters.
@@ -114,7 +67,7 @@ def venues():
   # TODO: replace with real venues data.
   #       num_upcoming_shows should be aggregated based on number of upcoming shows per venue.
   venues = Venue.query.all()
-  now = datetime.utcnow()
+  now = datetime.now()
   cities = []
   data = []
   for venue in venues:
@@ -148,7 +101,7 @@ def search_venues():
   search_term=request.form.get('search_term', '')
   venues= Venue.query.filter(Venue.name.ilike('%'+search_term+'%'))
   data =[]
-  now = datetime.utcnow()
+  now = datetime.now()
   for venue in venues:
     venue_details ={}
     venue_details['id'] = venue.id 
@@ -175,6 +128,7 @@ def show_venue(venue_id):
   now = datetime.now()
   venue_data = {}
   venue_data = venue.__dict__
+  venue_data['genres'] = venue_data['genres'].split(",")
   venue_data['upcoming_shows'] =[]
   venue_data['past_shows'] = []
   past_shows_count =0
@@ -223,7 +177,7 @@ def create_venue_submission():
     venue.state = request.form['state'] 
     venue.city =request.form['city']
     venue.address = request.form['address']
-    venue.phone =request.form['phone'].strip()
+    venue.phone = request.form['phone'].strip()
     venue.genres = (',').join(request.form.getlist('genres'))
     venue.facebook_link = request.form['facebook_link']
     venue.image_link = request.form['image_link']
@@ -337,6 +291,7 @@ def show_artist(artist_id):
   now = datetime.now()
   artist_data = {}
   artist_data = artist.__dict__
+  artist_data['genres'] = artist_data['genres'].split(",")
   artist_data['upcoming_shows'] =[]
   artist_data['past_shows']=[]
   past_shows_count =0
--- config.py
+++ config.py
@@ -10,4 +10,4 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL -
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres@localhost:5432/fyyurdb'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Real7325@localhost:5432/fyyurdb'
